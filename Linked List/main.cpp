15e24e9be2448b4cfa13700c76bf911651b07ff5
Morgan Chafe <mc.github@outlook.com>
2023-05-11 16:48:20 -0230

    updated

--- Linked List/main.cpp
+++ Linked List/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include "SinglyLinkedList.hpp"
 
 int main() 
 {
-    std::cout<<"Hello\n";
+    SinglyLinkedList<int> list(0);
+    for(size_t i = 1 ; i<11; i++)
+    {
+        list.add_to_end(2*i);
+        list.add_to_start(-2*i);
+    }
+
+    list.insert(9999,0);
+    list.insert(9999,list.length()-1);
+    list.insert(9999,list.length()/2);
+
+
+
+    
+
+    list.print();
+    std::cout<<"list.length= " << list.length() << "\n";
+
+    
+
+
 }
