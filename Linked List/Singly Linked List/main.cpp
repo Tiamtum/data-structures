aa8d3842068c2991036ed222334a6deab6bbe971
Morgan Chafe <mc.github@outlook.com>
2023-05-13 14:14:54 -0230

    tests

--- Linked List/Singly Linked List/main.cpp
+++ Linked List/Singly Linked List/main.cpp
@@ -3,32 +3,56 @@
 
 int main() 
 {
-    SinglyLinkedList<int> list(0);
-    list.add_to_start(345);
-    for(size_t i = 1 ; i<11; i++)
-    {
-        list.add_to_end(2*i);
-        list.add_to_start(-2*i);
-    }
-
-    list.insert(999,0);
-    list.insert(999,list.length()-1);
-    list.insert(999,list.length()/2);
-
-
-    std::cout<<"List length = " << list.length() << "\n";
-    list.print_values();
-
-    // size_t oldListLength = list.length();
-    // for(size_t i = 0; i<oldListLength-2; i++)
+    // SinglyLinkedList<int> list(3.14);
+    // list.add_to_start(345);
+    // for(size_t i = 1 ; i<11; i++)
     // {
-    //     list.delete_at(1);
+    //     list.add_to_end(2*i);
+    //     list.add_to_start(-2*i);
     // }
-    // std::cout<<"List length = " << list.length() << "\n";
-    // list.print_values();
 
-    SinglyLinkedList<int> list2(list);
-    std::cout<<"List2 length = " << list2.length() << "\n";
-    list2.print_values();
+    // list.insert(999,0);
+    // list.insert(999,list.length()-1);
+    // list.insert(999,list.length()/2);
+
+
+    // std::cout<<"List length = " << list.length() << "\n";
+    // // list.print_values();
+    // // list.print_list();
+
+    // SinglyLinkedList<int> list2(list);
+    // std::cout<<"List2 length = " << list2.length() << "\n";
+    // list2.print_list();
+
+    // SinglyLinkedList<int> list3(0);
+    // for(size_t i = 0 ; i<list2.length()-1; i++)
+    // {
+    //     list3.add_to_end(i+1);
+    // }
+    // list3.print_list();
+    // std::cout<<"List3 length = " << list3.length() << "\n";
+
+    // list3 = list2;
+    // list3.print_list();
+    
+    SinglyLinkedList<int> list1(1);
+    list1.add_to_end(2);
+    list1.add_to_end(3);
+    list1.add_to_end(4);
+    list1.add_to_end(5);
+    list1.add_to_end(6);
+
+    SinglyLinkedList<int> list2(0);
+    list2.add_to_end(-1);
+    list2.add_to_end(-2);
+    std::cout<<"=List1=\n";
+    list1.print_list();
+    std::cout<<"=List2=\n";
+    list2.print_list();
+    std::cout<<"list1=list2\n";
+    list1=list2;
+    list1.print_list();
+
+
 
 }
