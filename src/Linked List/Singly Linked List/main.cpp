533a9ea4edcb8efbab1e1afff09eac7868bc15b8
Morgan Chafe <mc.github@outlook.com>
2023-05-16 11:11:26 -0230

    Update main.cpp

--- src/Linked List/Singly Linked List/main.cpp
+++ src/Linked List/Singly Linked List/main.cpp
@@ -3,18 +3,22 @@
 #include <random>
 #include "SinglyLinkedList.hpp"
 
+int randInt(int from, int to)
+{    
+    std::random_device dev; //seed for the random number engine
+    std::mt19937 gen(dev()); //mersenne_twister_engine seeded with rd()
+    std::uniform_int_distribution<std::mt19937::result_type> dist(from,to);               
+    return dist(gen);
+}
+float randFloat(int from, int to)
+{
+    std::random_device dev;  
+    std::mt19937 gen(dev()); 
+    std::uniform_real_distribution<> dist(static_cast<float>(from), static_cast<float>(to));
+    return dist(gen);
+}
 
 int main() 
 {
-    SinglyLinkedList<int> list({1,2,3,4,5});
-
-
-    list.print_values();
-    list.swap_values(0,4);
-    std::cout<<"swapped\n";
-    list.print_values();
-    list.swap_values(4,0);
-    std::cout<<"swapped\n";
-    list.print_values();
-
+    SinglyLinkedList<int> list{1,2,3};
 }
