90d46ddf07b45b6b9077fdd12402c32db6987a85
Morgan Chafe <mc.github@outlook.com>
2023-05-16 17:42:49 -0230

    Update DoublyLinkedList.hpp

--- src/Linked List/Doubly Linked List/DoublyLinkedList.hpp
+++ src/Linked List/Doubly Linked List/DoublyLinkedList.hpp
@@ -1,6 +1,7 @@
 #ifndef DOUBLYLINKEDLIST_H
 #define DOUBLYLINKEDLIST_H
 
+#include <iostream>
 #include "Node.hpp"
 
 
@@ -9,30 +10,34 @@ class DoublyLinkedList
 {
     public:
     DoublyLinkedList() = delete; //handle empty case at some point
-    explicit DoublyLinkedList(T value) noexcept;
-    explicit DoublyLinkedList(std::initializer_list<T> values) noexcept;      
-    DoublyLinkedList(const DoublyLinkedList& list) noexcept;            //copy constructor (initialize a previously uninitialized list from some other list's data. )
-    DoublyLinkedList& operator=(const DoublyLinkedList& list) noexcept; //copy assignment (replace the data of a previously initialized list with some other list's data. )
-    DoublyLinkedList(DoublyLinkedList&& list) noexcept;                 //move constructor
-    DoublyLinkedList& operator=(DoublyLinkedList&& list) noexcept;      //move assignment
-    ~DoublyLinkedList();                                                //destructor
+    explicit DoublyLinkedList(T value) noexcept;    //TODO
+    explicit DoublyLinkedList(std::initializer_list<T> values) noexcept;    //TODO  
+    DoublyLinkedList(const DoublyLinkedList& list) noexcept;    //TODO            
+    DoublyLinkedList& operator=(const DoublyLinkedList& list) noexcept; //TODO 
+    DoublyLinkedList(DoublyLinkedList&& list) noexcept; //TODO                 
+    DoublyLinkedList& operator=(DoublyLinkedList&& list) noexcept;  //TODO      
+    ~DoublyLinkedList();                                                
 
-    void add_to_end(T value);
-    void add_to_start(T value);
-    void insert(T value, size_t index);
-    void delete_end();
-    void delete_start();
-    void delete_at(size_t index);
+    void add_to_end(T value);   //TODO
+    void add_to_start(T value); //TODO
+    void insert(T value, size_t index); //TODO
+    void delete_end();  //TODO
+    void delete_start();    //TODO
+    void delete_at(size_t index);   //TODO
 
-    void replace_value(size_t index,T value);
-    void swap_values(size_t index1, size_t index2);
-    void sort() requires std::is_arithmetic<T>::value;
-    T access(size_t index) const;
-    void print_list() const; 
-    void print_values() const;
-    T first_value() const;
-    T last_value() const;
-    size_t length() const;
+    void replace_value(size_t index,T value);   //TODO
+    void swap_values(size_t index1, size_t index2); //TODO
+    void sort() requires std::is_arithmetic<T>::value;  //TODO
+    T access(size_t index) const;   //TODO
+    void print_list() const;    //TODO 
+    void print_values() const;  //TODO
+    T first_value() const;  //TODO
+    T last_value() const;   //TODO
+    size_t length() const;  //TODO
+
+    template<typename U>
+    friend std::ostream& operator<<(std::ostream& os, const SinglyLinkedList<U>& other);
+
 
     private:
     Node<T> * walk_list_to_end(Node<T> * current) const;
@@ -41,5 +46,18 @@ class DoublyLinkedList
     Node<T> * m_head;
 };
 
+
+template<typename T>
+std::ostream& operator<<(std::ostream& os, const DoublyLinkedList<T>& other)
+{
+    Node<T> * current = other.m_head;
+    while(current->next)
+    {
+        os<<"Location: " << current << ", Value: " << current->value <<", Previous: " << current->previous <<", Next: " << current->next << "\n";
+        current = current->next;
+    }
+    os<<"Location: " << current << ", Value: " << current->value <<", Previous: " << current->previous <<", Next: " << current->next << "\n";
+    return os;
+}
 
 #endif 
