86065e560876307fe271da7bf305d5a3bd518579
Morgan Chafe <mc.github@outlook.com>
2023-05-17 14:29:21 -0230

    Create DoublyLinkedListTests.cpp

---
+++ src/Linked List/Doubly Linked List/Tests/DoublyLinkedListTests.cpp
@@ -0,0 +1,81 @@
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+#include "../../../../doctest/doctest/doctest.h"
+#include "../DoublyLinkedList.hpp"
+
+namespace DoublyLinkedListTests{
+
+};
+
+// TEST_CASE("Constructors")
+// {
+
+// }
+// TEST_CASE("Copy Assignment")
+// {
+
+//     SUBCASE("Assigning into a list of equal length")
+//     {
+
+//     }
+//     SUBCASE("Assigning into a list of smaller length")
+//     {
+      
+//     }
+//     SUBCASE("Assigning into a list of larger length")
+//     {
+
+//     }
+// }
+
+// TEST_CASE("Adding Nodes To Start and End")
+// {
+
+// }
+// TEST_CASE("Arbitrary Insertion Of Nodes")
+// {
+
+//     SUBCASE("Insert at start")
+//     {
+
+//     }
+//     SUBCASE("Insert at end")
+//     {
+
+//     SUBCASE("Insert at middle")
+//     {
+
+//     }
+
+// }
+
+// TEST_CASE("Deleting Nodes From Start and End")
+// {
+
+// }
+
+// TEST_CASE("Arbitrary Deletion of Nodes")
+// {
+
+//     SUBCASE("Delete at start")
+//     {
+
+//     } 
+//     SUBCASE("Delete at end")
+//     {
+
+//     }
+//     SUBCASE("Delete at middle")
+//     {
+
+//     }
+// }
+
+// TEST_CASE("Swap Values")
+// {
+
+// }
+
+// TEST_CASE("Other Member Functions")
+// {
+
+// }
