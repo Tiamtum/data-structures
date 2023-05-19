2eb903ce1d9ade1eb790ce7e64fc784d50ab7e22
Morgan Chafe <mc.github@outlook.com>
2023-05-19 14:11:47 -0230

    Update DoublyLinkedListTests.cpp

--- src/Linked List/Doubly Linked List/Tests/DoublyLinkedListTests.cpp
+++ src/Linked List/Doubly Linked List/Tests/DoublyLinkedListTests.cpp
@@ -99,11 +99,92 @@ namespace DoublyLinkedListTests{
     }
 }
 
+TEST_CASE("Default Constructor")
+{
+    DoublyLinkedList<int> list;
+    REQUIRE(list.length()==0);
+    SUBCASE("Adding nodes")
+    {
+        SUBCASE("To End")
+        {
+            list.add_to_end(0);
+            list.add_to_end(1);
+            list.add_to_end(2);
+            list.add_to_end(3);
+            list.add_to_end(4);        
+            DoublyLinkedList<int> expected({0,1,2,3,4});
+            REQUIRE(expected.length() == list.length());
+            CHECK(list==expected);
+        }
+        SUBCASE("To Start")
+        {
+            list.add_to_start(0);
+            list.add_to_start(-1);
+            list.add_to_start(-2);
+            list.add_to_start(-3);
+            list.add_to_start(-4);        
+            DoublyLinkedList<int> expected({-4,-3,-2,-1,0});
+            REQUIRE(expected.length() == list.length());
+            CHECK(list==expected);            
+        }
+    }
+    SUBCASE("Deleting nodes")
+    {
+        list.add_to_end(0);
+        list.add_to_end(1);
+        list.add_to_end(2);
+        list.add_to_end(3);
+        list.add_to_end(4);   
+        list.delete_end();  
+        list.delete_end();             
+        list.delete_end();             
+        list.delete_end();             
+        list.delete_end();             
+        CHECK(list.length() == 0);
+    }
+    SUBCASE("Assigning")
+    {
+        SUBCASE("Copy")
+        {
+            SUBCASE("Empty into empty")
+            {
+                DoublyLinkedList<int> empty;
+                empty=list;
+                CHECK(empty.length()==list.length());
+            }
+            SUBCASE("Empty into non-empty")
+            {
+                DoublyLinkedList<int> nonEmpty({1,2,3,4,5});
+                nonEmpty = list;
+                CHECK(nonEmpty.length()==0);
+            }
+            SUBCASE("Non-empty into empty ")
+            {
+                DoublyLinkedList<int> nonEmpty({1,2,3,4,5});
+                list = nonEmpty;
+                CHECK(list.length() == nonEmpty.length());
+            }
+        }
+        SUBCASE("Move")
+        {
+            DoublyLinkedList<int> list2({0,0,0,0,0});
+            list2 = std::move(list);
+            CHECK(list2.length()==0);            
+        }
+    }
+}
+
 TEST_CASE("Constructors")
 {
     CHECK(DoublyLinkedListTests::constructor_single_parameter() == true);
     CHECK(DoublyLinkedListTests::constructor_initializer_list_parameter() == true);
     CHECK(DoublyLinkedListTests::constructor_copy() == true);
+    SUBCASE("Empty Copy Construction")
+    {
+        DoublyLinkedList<int> list;
+        DoublyLinkedList<int> copiedFrom(list);
+        CHECK(copiedFrom.length() == list.length());
+    }
     CHECK(DoublyLinkedListTests::constructor_move() == true);
     CHECK(DoublyLinkedListTests::move_assignment() == true);
 }
