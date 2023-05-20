623cb373f18f606026285909f94cc8ca92cc7b24
Morgan Chafe <mc.github@outlook.com>
2023-05-20 13:36:51 -0230

    Update main.cpp

--- src/Stack/main.cpp
+++ src/Stack/main.cpp
@@ -1,9 +1,24 @@
-/**
- * @todo Implement
- * .
- */
+#include <iostream>
+#include <string>
+#include <random>
+#include "Stack.hpp"
 
-int main()
+int randInt(int from, int to)
+{    
+    std::random_device dev; //seed for the random number engine
+    std::mt19937 gen(dev()); //mersenne_twister_engine seeded with rd()
+    std::uniform_int_distribution<std::mt19937::result_type> dist(from,to);               
+    return dist(gen);
+}
+float randFloat(int from, int to)
 {
-    return 0;
+    std::random_device dev;  
+    std::mt19937 gen(dev()); 
+    std::uniform_real_distribution<> dist(static_cast<float>(from), static_cast<float>(to));
+    return dist(gen);
+}
+
+int main() 
+{
+
 }
